3bb033eede35a14155a361d2d987443ffce4431e
Toheeb Olanrewaju Olagoke <43994117+Olacode01@users.noreply.github.com>
2022-12-01 13:15:54 +0000

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+
+#Madlibs: Photo Shoot
+#Fill out these questions to generate your own silly mad 
+# libs story instantly online! (Hint: a Verb is an action. 
+# An adverb usually ends in “ly” and describes an action (like slowly). 
+# A noun is a person/place/thing. An adjective describes a person/place/thing.)
+
+
+Animal =input ("Enter Your favorite Animal")
+Feeling = input ("what is your feeling like")
+Things1 =input ("What is that tings ")
+Profession = input ("what is your favorite profession like")
+clothing = input("What is the clothing like? ")
+Things = input("what is the thing like? ")
+Person = input ("Your favorite person? ")
+Place = input ("Your favourite place to visit? ")
+verb = input ("Enter a verb")
+Food = input ("Enter your favorite food? ")
+
+myphotoshootmadlib =f"Say {Food} the photographer said as the camera flashed! \
+    {Person} and I had gone to {Place} to get our photos taken today. \
+    The first photo we really wanted was a picture of us dressed as {Animal} pretending to be a {Profession}\
+    When we saw the proofs of it, I was a bit {Feeling} because it looked different than in my head. \
+    (I hadn't imagined so many {Things1}behind us.) However, the second photo was exactly what I wanted. \
+    We both looked like {Things} wearing {clothing} and {verb} --exactly what I had in mind!"
+print = myphotoshootmadlib
+
